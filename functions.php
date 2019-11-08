ae0019f69726e4ae17c5508cfb243967f202a638
Nick Cartwright <dominic.cartwright07@gmail.com>
2019-11-08 13:52:51 -0500

    update formatting

--- functions.php
+++ functions.php
@@ -3,16 +3,18 @@
  * @package 	WordPress
  * @subpackage 	Charity NGO Child
  * @version		1.0.0
- * 
+ *
  * Child Theme Functions File
  * Created by CMSMasters
- * 
+ *
  */
 
 
 function charity_ngo_enqueue_styles() {
-    wp_enqueue_style('charity-ngo-child-style', get_stylesheet_uri(), array('charity-ngo-style'), '1.0.0', 'screen, print');
+
+	wp_enqueue_style( 'charity-ngo-child-style', get_stylesheet_uri(), array( 'charity-ngo-style' ), '1.0.0', 'screen, print' );
+
 }
 
-add_action('wp_enqueue_scripts', 'charity_ngo_enqueue_styles', 11);
+add_action( 'wp_enqueue_scripts', 'charity_ngo_enqueue_styles', 11 );
 ?>
